940c3390e0b47f333bd5643b686ce3bef03677a0
Shomit Mian <ssmian@usc.edu>
2024-09-20 13:37:33 -0700

    Update Person.cpp

--- lab3/part1/Person.cpp
+++ lab3/part1/Person.cpp
@@ -11,16 +11,16 @@ std::string Person::getName() {
 	return mName;
 }
 
-Professor::Professor(std::string name, std::string department) {
-	mName = name;
+Professor::Professor(std::string name, std::string department) : Person(name) {
+    mDepartment = department;
 }
 
 std::string Professor::getDepartment() {
 	return mDepartment;
 }
 
-Student::Student(std::string name, std::string major) {
-	mName = name;
+Student::Student(std::string name, std::string major) : Person(name) {
+    mMajor = major;
 }
 
 void Student::setGPA(double gpa) {
@@ -31,12 +31,14 @@ std::string Student::getMajor() {
 	return mMajor;
 }
 
-UscStudent::UscStudent(std::string name, std::string major, double gpa) {
-	mName = name;
-	mMajor = major;
-	setGPA(gpa);
-}
+UscStudent::UscStudent(std::string name, std::string major, double gpa) : Student(name, major) {
+    setGPA(gpa);
 
 void UscStudent::printTranscript() {
 	// TO DO: fill in
+	cout << "USC Transcript" << endl << "--------------------------" << endl; 
+	cout << "Name: " << getName() << endl; 
+	cout << "Major: " << getMajor() << endl;
+	cout << "GPA: " << mGpa << endl;
+	cout << "--------------------------" << endl;
 }
